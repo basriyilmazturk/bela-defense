98f3a448c99b5a31f97aedac0eaa61c5d5ac4e11
basriyilmazturk <basriyilmazturk@gmail.com>
2024-03-19 00:53:37 +0300

    feat: turret prefab generated

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -1,16 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Turret : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private Transform turretRotation;
+    [SerializeField] private LayerMask enemyMask;
 
     [Header("Attributes")]
     [SerializeField] private float targetingRange = 5f;
+    [SerializeField] private float rotationSpeed = 10f;
 
-    private void OnDrawGizmosSelected() {
-        Debug.Log("Gizmo Selected");
+    private Transform target;
+
+    private void Update()
+    {
+        if (target == null)
+        {
+            findTarget();
+            return;
+        }
+
+        RotateTowardsTarget();
+
+        if (!CheckTargetIsInRange())
+        {
+            target = null;
+        }
     }
+
+    private void OnDrawGizmosSelected()
+    {
+        Handles.color = Color.cyan;
+        Handles.DrawWireDisc(transform.position, transform.forward, targetingRange);
+    }
+
+    private bool CheckTargetIsInRange()
+    {
+        return Vector2.Distance(target.position, transform.position) < targetingRange;
+    }
+
+    private void findTarget()
+    {
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(
+            transform.position,
+            targetingRange,
+            (Vector2)transform.position,
+            0f,
+            enemyMask
+        );
+
+        if (hits.Length > 0)
+        {
+            target = hits[0].transform;
+        }
+    }
+
+    private void RotateTowardsTarget()
+    {
+        float angle = Mathf.Atan2(target.position.y - transform.position.y, target.position.x - transform.position.x) * Mathf.Rad2Deg;
+
+        Quaternion targetRotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+        turretRotation.rotation = Quaternion.RotateTowards(turretRotation.rotation, targetRotation, rotationSpeed * Time.deltaTime);// targetRotation;
+    }
+
+
 }
